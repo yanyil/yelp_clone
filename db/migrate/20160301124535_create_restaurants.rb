6a171d5c61acc74a1807f0bd2579939d9b07fa98
Yan-Yi Li <liyanyi@gmail.com>
2016-03-01 18:08:25 +0000

    display added restaurants

--- db/migrate/20160301124535_create_restaurants.rb
+++ db/migrate/20160301124535_create_restaurants.rb
@@ -2,8 +2,7 @@ class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.integer :ratinger
-      t.string :something
+      t.integer :rating
 
       t.timestamps null: false
     end

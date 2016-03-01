5ffe29573cdfeee63f2b04b0e66aedbca22117da
Yan-Yi Li <liyanyi@gmail.com>
2016-03-01 20:38:40 +0000

    viewing restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,12 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
+    @restaurant = Restaurant.new
   end
 
   def create

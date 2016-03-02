8e44177d775b3d5d984e29332d4a3d3b637115fc
Kait Beaudette <kait@kaitmakesthings.com>
2016-03-02 05:42:55 -0600

    Updated spec tests to full coverage, Added descriptions to Restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,47 @@
 class RestaurantsController < ApplicationController
 
-def index
-end
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = "Successfully deleted #{@restaurant.name}"
+    redirect_to restaurants_path
+  end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :rating)
+  end
 
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -11,13 +11,55 @@ feature 'restaurants' do
 
   context 'restaurants have been added' do
     before do
-      Restaurant.create(name: 'KFC')
+      Restaurant.create(name: 'KFC', description: 'very fancy restaurant')
     end
 
     scenario 'display restaurants' do
       visit '/restaurants'
       expect(page).to have_content('KFC')
+      expect(page).to have_content('very fancy restaurant')
       expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
+  context 'restaurants can be updated' do
+    before do
+      Restaurant.create(name: 'KFC', description: 'very fancy restaurant')
+    end
+
+    scenario 'update restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in "Name", with: 'Not that other place'
+      fill_in "Description", with: 'terribly fancy restaurant'
+      click_button 'Update'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Not that other place'
+    end
+  end
+
+  context 'restaurants can be deleted' do
+    before do
+      Restaurant.create(name: 'KFC', description: 'very fancy restaurant')
+    end
+
+    scenario 'delete restaurant' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).to have_content 'Successfully deleted KFC'
+    end
+  end
+
+  context 'restaurants have their own page' do
+
+    let!(:kfc) {Restaurant.create(name: 'KFC', description: 'very fancy restaurant')}
+
+    scenario 'show restaurant' do
+      visit '/restaurants'
+      click_link 'Show KFC'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+      expect(page).to have_content 'very fancy restaurant'
     end
   end
 end
